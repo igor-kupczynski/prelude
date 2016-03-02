5c445f65f87ca15e97019fca1c4f7a427002df48
Igor Kupczyński <igor@kupczynski.info>
2016-03-02 23:59:38 +0100

    Basic OSX emacs configuration

---
+++ personal/init.el
@@ -0,0 +1,7 @@
+;;; init.el --- generic customizations for emacs/prelude
+
+;;; Commentary:
+
+;;; Code:
+(setq prelude-guru nil)
+;;; init.el ends here
---
+++ personal/osx.el
@@ -0,0 +1,8 @@
+;;; osx.el --- Emacs modifications required for OSX
+
+;;; Commentary:
+
+;;; Code:
+(when (eq system-type 'darwin)
+  (setq mac-right-option-modifier 'none))
+;;; osx.el ends here
